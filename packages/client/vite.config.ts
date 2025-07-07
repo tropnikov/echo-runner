bf726588410fde398a388e6b5db7956187dee2d2
Maksim Tropnikov <max@tropnikov.dev>
2025-07-07 22:25:27 +0700

    fix(ECH-6): удалил неработающий vite-tsconfig-paths

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,8 @@
+import path from 'path';
+
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 dotenv.config();
 
@@ -13,5 +14,8 @@ export default defineConfig({
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
-  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  plugins: [react()],
 });

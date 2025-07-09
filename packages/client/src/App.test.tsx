e49116f37a88b73a184585135d4a15389c823ed3
eugene <eorlovsky@gmail.com>
2025-07-09 16:08:42 +0300

    fix example

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,10 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { RouterProvider } from 'react-router';
 
-import App from './App';
+import { render } from '@testing-library/react';
+
+import router from './routes';
 
 // @ts-ignore
 global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey') }));
 
 test('Example test', async () => {
-  render(<App />);
+  render(<RouterProvider router={router} />);
 });

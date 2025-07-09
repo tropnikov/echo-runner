9122cc4e984d4aa23a4eb3afddad611b73b1978f
eugene <eorlovsky@gmail.com>
2025-07-09 15:58:55 +0300

    use routing in navbar

---
+++ packages/client/src/constants/appRoutes.ts
@@ -0,0 +1,12 @@
+export const appRoutes = {
+  MAIN: '/',
+  TOPICS: 'topics',
+  TOPIC: ':topicId',
+  SIGNIN: 'signin',
+  SIGNUP: 'signup',
+  PROFILE: 'profile',
+  LEADERBOARD: 'leaderboard',
+  GAME: 'game',
+  ERROR: 'error',
+  NOT_FOUND: '*',
+};
--- packages/client/src/routes.tsx
+++ packages/client/src/routes.tsx
@@ -1,27 +1,28 @@
 import { createBrowserRouter } from 'react-router';
 
 import App from './App';
+import { appRoutes } from './constants/appRoutes';
 
 export default createBrowserRouter([
   {
-    path: '/',
+    path: appRoutes.MAIN,
     Component: App,
     children: [
       { index: true, Component: () => <div>Главная страница</div> },
       {
-        path: 'topics',
+        path: appRoutes.TOPICS,
         children: [
           { index: true, Component: () => <div>Форум</div> },
-          { path: ':topicId', Component: () => <div>Тема форума</div> },
+          { path: appRoutes.TOPIC, Component: () => <div>Тема форума</div> },
         ],
       },
-      { path: 'signin', Component: () => <div>Авторизация</div> },
-      { path: 'signup', Component: () => <div>Регистрация</div> },
-      { path: 'profile', Component: () => <div>Страница пользователя</div> },
-      { path: 'leaderboard', Component: () => <div>Лидерборд</div> },
-      { path: 'game', Component: () => <div>Игра</div> },
-      { path: 'error', Component: () => <div>Ошибка 500</div> },
-      { path: '*', Component: () => <div>Страница не найдена</div> },
+      { path: appRoutes.SIGNIN, Component: () => <div>Авторизация</div> },
+      { path: appRoutes.SIGNUP, Component: () => <div>Регистрация</div> },
+      { path: appRoutes.PROFILE, Component: () => <div>Страница пользователя</div> },
+      { path: appRoutes.LEADERBOARD, Component: () => <div>Лидерборд</div> },
+      { path: appRoutes.GAME, Component: () => <div>Игра</div> },
+      { path: appRoutes.ERROR, Component: () => <div>Ошибка 500</div> },
+      { path: appRoutes.NOT_FOUND, Component: () => <div>Страница не найдена</div> },
     ],
   },
 ]);

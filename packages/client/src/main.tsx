348ef2a3e1f3576b9dbe4866aa15429121b5ab4b
Eugene Orlovsky <e.orlovskiy@avoska.ru>
2025-07-25 12:34:26 +0300

    sw refactoring

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -12,20 +12,7 @@ import router from './routes';
 
 import './index.css';
 
-function startServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then((registration) => {
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        })
-        .catch((error: string) => {
-          console.log('ServiceWorker registration failed: ', error);
-        });
-    });
-  }
-}
+import startServiceWorker from './helpers/sw';
 
 startServiceWorker();
 

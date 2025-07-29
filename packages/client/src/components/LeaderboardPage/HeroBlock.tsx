f18cb9916fa59a6b286a216064e42c9cc0b9cec3
ismagilov_anton <oldvertu@gmail.com>
2025-07-29 20:50:51 +0700

    fix: correct url

--- packages/client/src/components/LeaderboardPage/HeroBlock.tsx
+++ packages/client/src/components/LeaderboardPage/HeroBlock.tsx
@@ -11,7 +11,7 @@ const HeroBlock: FC = () => {
   const navigate = useNavigate();
 
   const handleStartClick = () => {
-    navigate(`/${appRoutes.SIGNIN}`);
+    navigate(`/${appRoutes.GAME}`);
   };
 
   return (

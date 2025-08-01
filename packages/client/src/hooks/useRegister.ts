7ff2cee8b2ccb9421a528c2be3e0f1be88c0f95d
ismagilov_anton <oldvertu@gmail.com>
2025-08-01 20:40:09 +0700

    fix: use correct query

--- packages/client/src/hooks/useRegister.ts
+++ packages/client/src/hooks/useRegister.ts
@@ -1,23 +1,23 @@
 import { useCallback } from 'react';
 
-import { SignUpRequest, useGetAuthUserQuery, usePostAuthSignupMutation } from '@/api/generated';
+import { SignUpRequest, useLazyGetAuthUserQuery, usePostAuthSignupMutation } from '@/api/generated';
 import { setUser } from '@/redux/slices/auth';
 import { useAppDispatch } from '@/redux/store';
 
 export const useRegister = () => {
   const [registerMutation] = usePostAuthSignupMutation();
-  const { refetch } = useGetAuthUserQuery();
+  const [getAuthUser] = useLazyGetAuthUserQuery();
   const dispatch = useAppDispatch();
 
   const register = useCallback(
     async (values: SignUpRequest) => {
       await registerMutation({ signUpRequest: values }).unwrap();
-      const { data } = await refetch();
-      if (data) {
-        dispatch(setUser(data));
+      const user = await getAuthUser().unwrap();
+      if (user) {
+        dispatch(setUser(user));
       }
     },
-    [dispatch, registerMutation, refetch],
+    [dispatch, registerMutation, getAuthUser],
   );
 
   return { register };

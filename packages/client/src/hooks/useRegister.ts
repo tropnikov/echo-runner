a50e8ced7581603440dfcdad421dd0ed02629815
ismagilov_anton <oldvertu@gmail.com>
2025-08-01 16:28:02 +0700

    fix: login right after register

--- packages/client/src/hooks/useRegister.ts
+++ packages/client/src/hooks/useRegister.ts
@@ -1,15 +1,23 @@
 import { useCallback } from 'react';
 
-import { SignUpRequest, usePostAuthSignupMutation } from '@/api/generated';
+import { SignUpRequest, useGetAuthUserQuery, usePostAuthSignupMutation } from '@/api/generated';
+import { setUser } from '@/redux/slices/auth';
+import { useAppDispatch } from '@/redux/store';
 
 export const useRegister = () => {
   const [registerMutation] = usePostAuthSignupMutation();
+  const { refetch } = useGetAuthUserQuery();
+  const dispatch = useAppDispatch();
 
   const register = useCallback(
     async (values: SignUpRequest) => {
       await registerMutation({ signUpRequest: values }).unwrap();
+      const { data } = await refetch();
+      if (data) {
+        dispatch(setUser(data));
+      }
     },
-    [registerMutation],
+    [dispatch, registerMutation, refetch],
   );
 
   return { register };

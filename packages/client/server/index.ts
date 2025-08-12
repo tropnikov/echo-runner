1dbbf52cdbb967a1120bfd3d9607521d533144d2
Dmitriy <mocdmi@yandex.ru>
2025-08-12 23:54:18 +0300

    feat: Создан сервер.

--- packages/client/server/index.ts
+++ packages/client/server/index.ts
@@ -1,13 +1,9 @@
-import path from 'path';
-
 import dotenv from 'dotenv';
 import express from 'express';
 
 dotenv.config();
 
 const port = process.env.PORT || 80;
-const clientPath = path.join(__dirname, '../client');
-const isDev = process.env.NODE_ENV !== 'production';
 
 async function createServer() {
   const app = express();

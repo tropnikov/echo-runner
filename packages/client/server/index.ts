52b6d058c1d98ffedcdb2f511f3fbd6fd8eeba99
Dmitriy <mocdmi@yandex.ru>
2025-08-14 01:40:05 +0300

    feat: Настроена сборка.

--- packages/client/server/index.ts
+++ packages/client/server/index.ts
@@ -11,7 +11,7 @@ import { RenderResult } from './types';
 dotenv.config();
 
 const port = process.env.PORT || 3000;
-const clientPath = path.join(__dirname, '../');
+const clientPath = path.join(__dirname, '../../');
 
 async function createServer() {
   const app = express();

3548db14eb73690b23d5801925f5a311d4e36730
Yegor Twin <yegor@MacBook-Air-Yegor.local>
2025-08-29 16:52:48 +0300

    feat(ECH-48): добавлен middleware для авторизации

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,35 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 
 import { createClientAndConnect } from './db';
+import { authMiddleware } from './middlewares/authMiddleware';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(cookieParser());
+
 const port = Number(process.env.SERVER_PORT) || 3001;
+
+app.use((req, res, next) => {
+  const publicPaths = ['/', '/signin', '/signup', '/oauth'];
+  if (publicPaths.includes(req.path)) {
+    return next();
+  }
+  return authMiddleware(req, res, next);
+});
 
 createClientAndConnect();
 
 app.get('/', (_, res) => {
   res.json('👋 Howdy from the server :)');
+});
+
+app.get('/private', (_req, res) => {
+  res.json({ ok: true });
 });
 
 app.listen(port, () => {

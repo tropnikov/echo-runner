4a5db0be3d70909089cb5fcd10f1319678b601c4
Yegor Twin <yegor@MacBook-Air-Yegor.local>
2025-08-29 19:20:59 +0300

    fix(ECH-48): сменён код ответа с 401 на 403

--- packages/server/middlewares/authMiddleware.ts
+++ packages/server/middlewares/authMiddleware.ts
@@ -1,12 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
-// простой вариант: проверяем только наличие куки
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const authCookie = req.cookies?.authCookie;
 
   if (!authCookie) {
-    return res.status(401).json({ reason: 'Unauthorized' });
-    return;
+    return res.status(403).json({ reason: 'Unauthorized' });
   }
 
   return next();
